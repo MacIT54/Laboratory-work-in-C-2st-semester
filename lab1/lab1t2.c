d102c0e1f2d823fd07ab7d9451d1348bf4de69ed
MacIT54 <daniil.ledkin@mail.ru>
2023-02-01 15:46:06 +0600

    Laboratory work in C

--- lab1/lab1t2.c
+++ lab1/lab1t2.c
@@ -17,6 +17,8 @@ int main() {
     if (n > 0 || n < 0) {
         float degree = degree_of_number(n, x);
         printf("Number %f to the degree of %d: %f\n", x, n, degree);
+    } else if (n == 0) {
+        printf("Number %f to the degree of %d: %d\n", x, n, 1);
     } else {
         printf("The number does not match the condition\n");
     }
